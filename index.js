8d910e1668d7b56cb2197a1bd83752db569b526e
Горин Пётр <peter14102005@gmail.com>
2024-02-02 15:00:45 +0300

    Still a Work in Progress

--- index.js
+++ index.js
@@ -6,37 +6,61 @@ const EmployeeDB = {
     //методы для добавления, удаления, обновления и чтения сотрудников.
     
     addEmployee: function (name, position, department) {
-        const employee = {
-            name: name,
-            position: position,
-            department: department
-        };
-        this.employees.push(employee)
-    },
+    const employee = {
+      name: name,
+      position: position,
+      department: department
+    };
+    this.employees.push(employee);
+  },
 
-    removeEmployee: function (index) {
-        this.employees.splice (index, 1)
-    },
+  removeEmployee: function (index) {
+    this.employees.splice(index, 1);
+  },
 
-    updateEmployee: function (index, newName, newPosition, newDepartment) {
-        this.employees[index].name = newName;
-        this.employees[index].position = newPosition;
-        this.employees[index].department = newDepartment;
-    },
+  updateEmployee: function (index, newName, newPosition, newDepartment) {
+    this.employees[index].name = newName;
+    this.employees[index].position = newPosition;
+    this.employees[index].department = newDepartment;
+  },
 
-    displayEmployee: function () {
-        this.employee.forEach(function (employee) {
-          console.log("Name: " + employee.name);
-          console.log("Position: " + employee.position);
-          console.log("Department: " + employee.department);
-          console.log("----------------------");
-        });
-    }
-}
+  displayEmployees: function () {
+    this.employees.forEach(function (employee) {
+      console.log("Name: " + employee.name);
+      console.log("Position: " + employee.position);
+      console.log("Department: " + employee.department);
+      console.log("----------------------");
+    });
+  }
+};
 
 //Добавление нескольких сотрудников в базу данных
-EmployeeDB.addEmployee ("", "", "");
-EmployeeDB.addEmployee ("", "", "");
+EmployeeDB.addEmployee ("David Leonard", "Game Developer", "Level Designer");
+EmployeeDB.addEmployee ("Jim Lawrence", "Doctor", "Blood Research");
+EmployeeDB.addEmployee ("Aaron Drew", "Cartoonist", "Storyboard Artist");
+EmployeeDB.addEmployee ("Joseph Dallas", "Musician", "Pianist");
 
 //Чтение данных 
-EmployeeDB.displayEmployee();
+EmployeeDB.displayEmployees();
+
+//Модифицируем данные
+
+EmployeeDB.capitaliseNames = function () {
+    this.employees.forEach(function (employee) {
+      employee.name = employee.name.toUpperCase();
+    });
+};
+  
+EmployeeDB.convertToLowerCase = function () {
+    this.employees.forEach(function (employee) {
+        employee.department = employee.department.toLowerCase();
+    });
+};
+  
+EmployeeDB.capitaliseNames();
+EmployeeDB.convertToLowerCase();
+EmployeeDB.displayEmployees();
+
+EmployeeDB.removeEmployee(1);
+EmployeeDB.updateEmployee(0, "Doctor Eggman", "Scientist", "Evil");
+EmployeeDB.displayEmployees();
